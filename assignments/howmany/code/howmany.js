40b280910ced8a9286893419b43f9c799f385c8f
METROSTATE\36BDPA3 <tristanlyx@gmail.com>
2017-03-18 13:45:17 -0500

    game WIP

--- assignments/howmany/code/howmany.js
+++ assignments/howmany/code/howmany.js
@@ -1,16 +1,53 @@
 function doLevel1() {
-	alert("Is this right?");
+    var userAnswer = prompt("Write your answer down below.");
+	var correctAnswer1 = ("satellites");
+	if (userAnswer == correctAnswer1){
+		alert("That's right!"); addPoints(10);
+	}else{
+		alert("You got it wrong, the answer was " + correctAnswer1 + "." ); addPoints(-10);
+		}			
 	hide("level1"); 
 	show("level2");
 }
 function doLevel2(){
-	alert("This is correct?");
+	var userAnswer = prompt("Write your answer down below.");
+	var correctAnswer2 = (60);
+	correctAnswer2 = Number(correctAnswer2);
+	if (userAnswer == correctAnswer2){
+		alert("That's right!"); addPoints(10);
+	}else{
+		alert("You got it wrong, the answer was " + correctAnswer2 + "."); addPoints(-10);
+		}
 	hide("level2");
 	show("level3");
 }
 function doLevel3(){
-	alert("I'm pretty sure this is okay...");
+	var userAnswer = prompt("Write your answer down below.");
+	var correctAnswer3 = "true"
+	if (userAnswer == correctAnswer3){
+		alert("That's right!"); addPoints(10);
+		}else{
+		alert("You got it wrong, the answer was " + correctAnswer3 + "."); 
+		addPoints(-10);
+		}
 	hide("level3");
+	show("finish");
+}
+function doNext(){
+	hide("finish");
+	show("level4");
+	}
+function showScore() {
+	var finalScore
+	finalScore = "Your final score is " + score;
+	alert(finalScore);
+}
+function doStage1() {
+	var userAnswer = prompt("Write your answer here.");
+	var correctAnswer = ("Dumbo");
+	if (userAnswer == correctAnswer) {
+		alert("That's right!"); 
+	}
 }
 function show(id) {
 	var el = document.getElementById(id);
