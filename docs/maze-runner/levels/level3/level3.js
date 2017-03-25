d75e2f15ec050feaa34e4e35532a9fdd9166ab61
David Allender <david.a.allender@gmail.com>
2017-03-25 00:06:54 -0500

    TODO revert me. This changeset removes the answers from the docs directory

--- docs/maze-runner/levels/level3/level3.js
+++
@@ -1,9 +0,0 @@
-function runLevel(){
-    var cabbageMan = new MazeRunner.CabbageMan();
-    cabbageMan.moveRight();
-    cabbageMan.moveUp();
-    cabbageMan.moveRight();
-    cabbageMan.moveRight();
-    cabbageMan.moveDown();
-    cabbageMan.moveRight(16);
-}
