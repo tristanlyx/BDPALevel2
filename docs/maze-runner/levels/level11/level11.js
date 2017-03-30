e66a15d4209ebe73c8122dc45e9496bb9ea98980
David Allender <david.a.allender@gmail.com>
2017-03-29 19:54:07 -0500

    level shift and re-implement

--- docs/maze-runner/levels/level11/level11.js
+++ docs/maze-runner/levels/level11/level11.js
@@ -1,34 +1,21 @@
 
 
 function decidePassword(prompt){
-    if(prompt == "CYAN"){
-        return "NAYC";
-    } else if (prompt == "PINK") {
-        return "KNIP";
-    } else if (prompt == "RED"){
-        return "DER";
-    } else if (prompt=="BLUE"){
-        return "EULB";
-    } else if (prompt == "GRAY"){
-        return "YARG"
-    }
+    return (prompt + 1) / 2
 }
 
 function runLevel(){
     var cabbageMan = new MazeRunner.CabbageMan();
+    cabbageMan.moveRight(1);
+    cabbageMan.moveUp(9);
+    cabbageMan.calculatePassword("RIGHT", decidePassword);
+    cabbageMan.moveRight(5);
+    cabbageMan.calculatePassword("RIGHT", decidePassword);
+    cabbageMan.moveRight(5);
+    cabbageMan.calculatePassword("RIGHT", decidePassword);
+    cabbageMan.moveRight(5);
+    cabbageMan.calculatePassword("RIGHT", decidePassword);
     cabbageMan.moveRight(2);
-    cabbageMan.moveUp();
-    cabbageMan.calculatePassword("UP", decidePassword);
-    cabbageMan.moveUp();
-    cabbageMan.moveRight();
-    cabbageMan.calculatePassword("RIGHT", decidePassword);
-    cabbageMan.moveRight(4);
-    cabbageMan.calculatePassword("RIGHT", decidePassword);
-    cabbageMan.moveRight(4);
-    cabbageMan.calculatePassword("DOWN", decidePassword);
-    cabbageMan.moveDown();
-    cabbageMan.moveRight(2);
-    cabbageMan.moveDown();
-    cabbageMan.calculatePassword("RIGHT", decidePassword);
-    cabbageMan.moveRight(6);
+    cabbageMan.moveDown(9);
+    cabbageMan.moveRight(1);
 }
